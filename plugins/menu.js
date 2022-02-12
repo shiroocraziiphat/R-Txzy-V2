28efb8c854c00dc9428f49a59426b03f8c5ae715
LANxBOT <98082226+LANxBOT@users.noreply.github.com>
2022-02-13 04:36:54 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -349,7 +349,7 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
                             }, {
                                 "rows": [{
                                     "title": "Owner bot",
-                                    "description": "pemilik R-Txzy",
+                                    "description": "pemilik LynXzy",
                                     "rowId": ".owner"
                                 }, {
                                     "title": "Donasi",
