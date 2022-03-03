d6ea1c938c11a8972a4fa5a21d70972be55b51bb
LANxBOT <98082226+LANxBOT@users.noreply.github.com>
2022-03-03 23:49:34 +0800

    Update R-Txzy.js

--- plugins/R-Txzy.js
+++ plugins/R-Txzy.js
@@ -1,6 +1,6 @@
 let handler  = async (m, { conn, usedPrefix: _p }) => {
 ye = `@${m.sender.split`@`[0]}`
-let info = `https://github.com/LANxBOT/R-Txzy-V2  thanks mana?`.trim()
+let info = `https://github.com/LANxBOT/R-Txzy-V2/n thanks mana?`.trim()
 
 conn.fakeReply(m.chat, info, '0@s.whatsapp.net', `*${global.packname}*`, 'status@broadcast') 
 }
