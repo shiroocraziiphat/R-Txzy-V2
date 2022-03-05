bbf252a2a16aafa938a24e2ef4ae2dd656e1de37
LANxBOT <98082226+LANxBOT@users.noreply.github.com>
2022-03-05 19:48:37 +0800

    Update leaderboard.js

--- plugins/leaderboard.js
+++ plugins/leaderboard.js
@@ -14,19 +14,19 @@ let handler = async (m, { conn, args, participants }) => {
 ╭═══ 〔 *XP Leaderboard Top ${len}* 〕
 ║│➸ Kamu: *${usersExp.indexOf(m.sender) + 1}* dari *${usersExp.length}*
 ║│
-║│➸${sortedExp.slice(0, len).map(({ jid, exp }, i) => `├ ${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${exp} Exp*`).join`\n`}
+${sortedExp.slice(0, len).map(({ jid, exp }, i) => `║│➸ ${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${exp} Exp*`).join`\n`}
 ║╰──────────────────
 ╰═══════════════════
 ╭═══ 〔 *Limit Leaderboard Top ${len}* 〕
 ║│➸ Kamu: *${usersLim.indexOf(m.sender) + 1}* dari *${usersLim.length}*
 ║│
-║│➸${sortedLim.slice(0, len).map(({ jid, limit }, i) => `├ ${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${limit} Limit*`).join`\n`}
+${sortedLim.slice(0, len).map(({ jid, limit }, i) => `║│➸ ${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${limit} Limit*`).join`\n`}
 ║╰──────────────────
 ╰═══════════════════
 ╭═══ 〔 *Level Leaderboard Top ${len}* 〕
 ║│➸ Kamu: *${usersLevel.indexOf(m.sender) + 1}* dari *${usersLevel.length}*
 ║│
-║│➸${sortedLevel.slice(0, len).map(({ jid, level }, i) => `├ ${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *Level ${level}*`).join`\n`}
+${sortedLevel.slice(0, len).map(({ jid, level }, i) => `║│➸ ${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *Level ${level}*`).join`\n`}
 ║╰──────────────────
 ╰═══════════════════`.trim()
   conn.reply(m.chat, text, m, {
